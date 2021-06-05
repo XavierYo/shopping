0adb2120779d82d0c64ce05eeaae2ba122fe8802
Xavier <yang101225@qq.com>
2021-06-05 15:24:07 +0800

    使用BeanUtils简化封装

--- src/com/xavier/web/servlet/ModItemServlet.java
+++ src/com/xavier/web/servlet/ModItemServlet.java
@@ -1,9 +1,11 @@
 package com.xavier.web.servlet;
 
 import com.xavier.domain.Item;
+import com.xavier.domain.User;
 import com.xavier.service.ItemService;
 import com.xavier.service.impl.CategoryServiceImpl;
 import com.xavier.service.impl.ItemServiceImpl;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 @WebServlet("/ModItemServlet")
@@ -19,60 +22,34 @@ public class ModItemServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收数据:
         request.setCharacterEncoding("utf-8");
-        String item_name = request.getParameter("item_name");//商品名字
-
-        String temp_price = request.getParameter("price");//商品价格，需转换为double
-        double price=0;
-        if(temp_price == null || temp_price.equals("")) {
-            price = 0;
-        } else {
-            price = Double.parseDouble(temp_price);
+        Item item = new Item();
+        try {
+            BeanUtils.populate(item, request.getParameterMap());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
 
-        String  temp_number = request.getParameter("number");
-        int number=0;
-        if(temp_number==null||temp_number.equals("")){
-            number=0;
-        }else{
-            number = Integer.parseInt(temp_number);
-        }
-        //商品类别id
-        String temp_CatName = request.getParameter("category_name");
         CategoryServiceImpl categoryService=new CategoryServiceImpl();
-        int category_id=-1;
-
+        User seller=(User)request.getSession().getAttribute("existUser");
+        // 判断是否具有该类别的权限
         try{
-            category_id=categoryService.getCatID(temp_CatName);
-            //-1即表示类别不存在，需创建
-            if(category_id==-1){
-                categoryService.insCategory(temp_CatName);
-                category_id=categoryService.getCatID(temp_CatName);
+            if(categoryService.isInCharge(seller.getUser_id(),item.getCategory_id())==false){
+                request.setAttribute("msg","你没有该类别的权限！");
+                request.getRequestDispatcher("/mod_item.jsp").forward(request, response);
+                return;
             }
         }catch (SQLException e){
             e.printStackTrace();
         }
 
-        String desc = request.getParameter("description");//商品描述
-        // 封装数据:
-        Item new_item = new Item();
-        new_item.setItem_name(item_name);
-        new_item.setPrice(price);
-        new_item.setCategory_id(category_id);
-        new_item.setDescription(desc);
-        new_item.setNum(number);
 
         // 处理数据:完成插入
         ItemService itemService = new ItemServiceImpl();
-        int item_id= 0;
-        try {
-            item_id = itemService.getItemByName(item_name).getItem_id();
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-        }
-        new_item.setItem_id(item_id);
         boolean flag= false;
         try {
-            flag = itemService.modItem(new_item);
+            flag = itemService.modItem(item);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- web/mod_item.jsp
+++ web/mod_item.jsp
@@ -1,3 +1,4 @@
+<%@ page import="com.xavier.service.impl.ItemServiceImpl" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
@@ -5,17 +6,23 @@
 </head>
 <body>
 <%@include file="header.jsp"%>
+<%
+    ItemServiceImpl itemService=new ItemServiceImpl();
+    request.getSession().setAttribute("items_searchRes",itemService.getAllItems());
+%>
 <form  onsubmit="return checkForm()" method="post" action="${pageContext.request.contextPath }/ModItemServlet">
     <fieldset style="display:inline-block;*display:inline;*zoom:1;">
-        <legend>修改商品</legend>
-    商品名称: <input type="text" name="item_name" value=""><br>
-    商品价格: <input type="number" name="price" value="Mouse"><br>
-    商品数目: <input type="number" name="number" value=""><br>
-    商品类别: <input type="text" name="category_name" value=""><br>
-    商品描述: <input type="text" name="description" value=""><br>
+        <legend>修改商品-（商品id是唯一标识）</legend>
+        商品ID: <input type="number" name="item_id" value=""><br>
+        商品名称: <input type="text" name="item_name" value=""><br>
+        商品价格: <input type="number" name="price" value=""><br>
+        商品数目: <input type="number" name="num" value=""><br>
+        类别ID: <input type="text" name="category_id" value=""><br>
+        商品描述: <input type="text" name="description" value=""><br>
     <input type="submit" value="提交">
     </fieldset>
 </form>
+<%@include file="showitems.jsp"%>
 <%
     String msg=(String)request.getAttribute("msg");
     if(msg!=null)
